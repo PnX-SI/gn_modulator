8a90a46081a330cabea33c2fdbf0688f683acab8
joel.clement <joelclems@gmail.com>
2022-03-08 09:31:57 +0100

    marker leaflet

--- frontend/app/services/map.service.ts
+++ frontend/app/services/map.service.ts
@@ -14,6 +14,21 @@ L.PM.initialize({ optIn: true }); // Property 'PM' does not exist on type 'typeo
 delete L.Icon.Default.prototype['_getIconUrl'];
 L.Icon.Default.mergeOptions(CustomIcon);
 
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 import mapMethods from './map'
 
 @Injectable()
