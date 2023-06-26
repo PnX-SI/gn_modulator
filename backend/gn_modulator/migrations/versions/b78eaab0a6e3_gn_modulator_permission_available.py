7be876d93c340180f984a77d4708e26295491ffa
joel.clement <joelclems@gmail.com>
2023-06-26 11:22:08 +0200

    remove bad permissions

--- backend/gn_modulator/migrations/versions/b78eaab0a6e3_gn_modulator_permission_available.py
+++ backend/gn_modulator/migrations/versions/b78eaab0a6e3_gn_modulator_permission_available.py
@@ -7,7 +7,7 @@ Create Date: 2023-06-20 15:19:21.097194
 """
 from alembic import op
 import sqlalchemy as sa
-
+from gn_modulator import MODULE_CODE
 
 # revision identifiers, used by Alembic.
 revision = "b78eaab0a6e3"
@@ -45,6 +45,36 @@ INSERT INTO
             gn_permissions.t_objects o ON o.code_object = v.object_code
         JOIN
             gn_permissions.bib_actions a ON a.code_action = v.action_code
+    """
+    )
+
+    op.execute(
+        f"""
+        WITH bad_permissions AS (
+            SELECT
+                p.id_permission
+            FROM
+                gn_permissions.t_permissions p
+            JOIN gn_commons.t_modules m
+                    USING (id_module)
+            WHERE
+                m.module_code IN ('{MODULE_CODE}')
+            EXCEPT
+            SELECT
+                p.id_permission
+            FROM
+                gn_permissions.t_permissions p
+            JOIN gn_permissions.t_permissions_available pa ON
+                (p.id_module = pa.id_module
+                    AND p.id_object = pa.id_object
+                    AND p.id_action = pa.id_action)
+        )
+        DELETE
+        FROM
+            gn_permissions.t_permissions p
+                USING bad_permissions bp
+        WHERE
+            bp.id_permission = p.id_permission;
     """
     )
 
