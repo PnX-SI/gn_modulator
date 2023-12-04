b5a6f3d7c52960d3d7dc3c4aa508903b5699450a
joel.clement <joelclems@gmail.com>
2023-12-04 16:34:56 +0100

    add diagnostic scope / permissions

--- contrib/m_sipaf/backend/m_sipaf/permission.py
+++ contrib/m_sipaf/backend/m_sipaf/permission.py
@@ -1,6 +1,6 @@
 import sqlalchemy as sa
 from pypnusershub.db.models import User
-from .models import PassageFaune, Actor
+from .models import PassageFaune, Actor, Diagnostic
 from geonature.utils.env import db
 
 
@@ -53,5 +53,33 @@ def passage_faune_permission_filter(cls, query, id_role, scope_for_action, sensi
         return None, query
 
 
+@classmethod
+def diagnostic_subquery_scope(cls, id_role):
+    scope_expression_passage_faune = PassageFaune.subquery_scope(id_role).cte("pre_scope_pf")
+    scope_expression = (
+        db.session.query(Diagnostic)
+        .join(
+            scope_expression_passage_faune,
+            scope_expression_passage_faune.c.id_passage_faune == Diagnostic.id_passage_faune,
+        )
+        .with_entities(Diagnostic.id_diagnostic, scope_expression_passage_faune.c.scope)
+    )
+
+    return scope_expression
+
+
+@classmethod
+def diagnostic_permission_filter(cls, query, id_role, scope_for_action, sensitivity):
+    if scope_for_action < 3:
+        query = query.add_subquery_scope(id_role)
+        return query._subquery_scope.c.scope <= scope_for_action, query
+    else:
+        return None, query
+
+
 PassageFaune.subquery_scope = passage_faune_subquery_scope
 PassageFaune.permission_filter = passage_faune_permission_filter
+
+
+Diagnostic.subquery_scope = diagnostic_subquery_scope
+Diagnostic.permission_filter = diagnostic_permission_filter
